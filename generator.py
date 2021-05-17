8f7dea2cc3a262366cac0a57fe38b6d9a6b6a87d
james <james@jamess-MacBook-Pro.local>
2021-05-17 17:29:20 +0100

    add save_wav method to stream object, some more to-do comments in modules

--- generator.py
+++ generator.py
@@ -2,13 +2,27 @@ import numpy as np
 import stream
 import notes
 
+# TO DO:
+# - Ultimately have Synth and Sampler classes that own their own stream (stream.py) object
+#   allowing ADSR volume and filter enveloping, LFO implementation etc.
+# - Functions here will generally be called from a "Score" class that is provided with the
+#   musical choices and uses these to generate sound, but can be interfaced with directly.
+
 def gen_chord(stream, chordname, rootoctv=3):
+    """ 
+    generate chord over entire stream given chord name and optional
+    octave of root note
+    """
     frqs = notes.parse_chord(chordname, rootoctv)
     frqsamp = frqs/stream.samprate 
     for f in frqsamp:
         stream.values += detuned_saw(stream.samples, f)
     
 def detuned_saw(samples, freqsamp, oscdets=[1,1.005,0.995]):
+    """
+    Three oscillator sawtooth wave generator with slight detuning for
+    
+    """
     saw = lambda freqsamp, samp: 1-((samples*(freqsamp)/2) % 2)
     signal = np.zeros(samples.size)
     for det in oscdets:
