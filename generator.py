fc8109f0f4d938cc0f89b2db507d09a29b23e973
James Trayford <jamestrayford@Jamess-MacBook-Pro.local>
2021-07-13 15:37:28 +0100

    add simple synthesizer with sample loading as interpolation objects

--- generator.py
+++ generator.py
@@ -1,6 +1,9 @@
 import numpy as np
 import stream
 import notes
+import glob
+import wavio
+from scipy.interpolate import interp1d
 
 # TO DO:
 # - Ultimately have Synth and Sampler classes that own their own stream (stream.py) object
@@ -21,7 +24,7 @@ def gen_chord(stream, chordname, rootoctv=3):
 def detuned_saw(samples, freqsamp, oscdets=[1,1.005,0.995]):
     """
     Three oscillator sawtooth wave generator with slight detuning for
-    
+    texture
     """
     saw = lambda freqsamp, samp: 1-((samples*(freqsamp)/2) % 2)
     signal = np.zeros(samples.size)
@@ -29,3 +32,36 @@ def detuned_saw(samples, freqsamp, oscdets=[1,1.005,0.995]):
         freq = freqsamp*det
         signal += saw(freq, samples+freq*np.random.random())
     return signal
+
+class Generator:
+    pass
+
+class Synthesizer(Generator):
+    pass
+
+class Sampler(Generator):
+    def __init__(self, sampfiles, params=None):
+        if isinstance(sampfiles, dict):
+            self.sampdict = sampfiles
+        if isinstance(sampfiles, str):
+            wavs = glob.glob(sampfiles+"/*")
+            self.sampdict = {}
+            for w in wavs:
+                note = w.split('/')[-1].split('_')[-1].split('.')[0]
+                self.sampdict[note] = w
+        self.load_samples()
+        
+    def load_samples(self):
+        self.samples = {}
+        self.samplens = {}
+        for note in self.sampdict.keys():
+            wavobj = wavio.read(self.sampdict[note])
+            # force to mono
+            wavdat = wavobj.data.mean(axis=1)
+            wavdat /= abs(wavdat).max()
+            samps = range(wavdat.size)
+            self.samples[note] = interp1d(samps, wavdat,
+                                          bounds_error=False,
+                                          fill_value = (0.,0.),
+                                          assume_sorted=True)
+            self.samplens[note] = wavdat.size
