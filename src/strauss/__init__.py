a03e9ea1abf8fede2379ad6b42427b9b7a7754f4
James Trayford <jamestrayford@Jamess-MacBook-Pro.local>
2021-08-07 21:26:22 +0100

    add relative imports to top __init__

---
+++ src/strauss/__init__.py
@@ -0,0 +1,11 @@
+# relative imports of submodules
+
+from . import channels
+from . import filters
+from . import generator
+from . import notes
+from . import score
+from . import sonification
+from . import sources
+from . import stream
+from . import presets
