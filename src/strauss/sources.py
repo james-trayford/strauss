f6e0794f1396ca939f874f6a75b642d411392a01
James Trayford <jamestrayford@Jamess-MacBook-Pro.local>
2021-08-23 23:34:07 +0100

    add functionality for objects and single object stream

--- src/strauss/sources.py
+++ src/strauss/sources.py
@@ -1,12 +1,15 @@
 import numpy as np
 import pandas as pd
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
 
 mappable = ['theta',
             'phi',
             'volume',
             'pitch',
             'time',
-            'filt']
+            'cutoff',
+            'time_evo']
 
 class Source:
     """ Generic source class """
@@ -21,8 +24,9 @@ class Source:
         self.mapped_quantities = mapped_quantities
         self.raw_mapping = {}
         self.mapping = {}
+        self.mapping_evo = {}
         
-    def apply_mapping_functions(self, map_funcs, map_lims):
+    def apply_mapping_functions(self, map_funcs=None, map_lims=None):
         for key in self.mapped_quantities:
             rawvals = self.raw_mapping[key]
 
@@ -39,24 +43,66 @@ class Source:
                     if pc > 100:
                         buff = pc/100.
                         pc = 100
-                    lim = np.percentile(mapvals, pc)*buff
+                    lim = np.percentile(np.hstack([mapvals]), pc)*buff
                     lims.append(lim)
                 else:
                     # numerical values notate absolute limits
                     lims.append(l)
     
-            mapvals = (mapvals - lims[0]) / np.diff(lims)
-
-            # finally, construct mapped values from 0 to 1
-            self.mapping[key] = np.clip(mapvals, 0, 1)
-    
+            # limit mapped values from 0 to 1 NOTE: do we want to mix and match const and evo?
+            if hasattr(mapvals[0], "__iter__"):
+                self.mapping[key] = []
+                for i in range(self.n_sources):
+                    mapvals = (mapvals[i] - lims[0]) / np.diff(lims)
+                    self.mapping[key].append(np.clip(mapvals,0,1))
+            else:
+                mapvals = (mapvals - lims[0]) / np.diff(lims)
+                self.mapping[key] =  np.clip(mapvals, 0, 1)
+                
+        # finally, iterate through sources and interpolate evo functions 
+        for key in self.mapping:
+            if key == "time_evo":
+                continue
+            elif hasattr(self.mapping[key][0], "__iter__"):
+                for i in range(self.n_sources):
+                    x = self.mapping["time_evo"][i]
+                    y = self.mapping[key][i]
+                    if key == "phi":
+                        # special case: shortest angular distance
+                        # between phi points is always assumed
+                        ydiff = np.diff(y)
+                        discont_bdx = abs(ydiff) > 0.5
+                        for j in range(discont_bdx.sum()):
+                            xpre = x[:-1][discont_bdx][j]
+                            ysense = np.sign(ydiff[discont_bdx][j]) 
+                            y[x > xpre] -= ysense
+                    self.mapping[key][i] = interp1d(x,y, bounds_error=False,
+                                                    fill_value=(y[0],y[-1]))
             
 class MultiEvents(Source):
     def fromfile(self, datafile, mapdict):
         data = np.genfromtxt(datafile)
         for key in self.mapped_quantities:
             self.raw_mapping[key] = data[:,mapdict[key]] 
-        self.nevents = data.shape[0]
+        self.n_sources = data.shape[0]
+        
+    def fromdict(self, datadict):
+        for key in self.mapped_quantities:
+            if key in datadict:
+                self.raw_mapping[key] = datadict[key]
+            else:
+                Exception(f"Mapped property {key} not in datadict.")
+        self.n_sources = datadict[key].shape[0]
+ 
+class SingleObject(Source):
+    def fromdict(self, datadict):
+        for key in self.mapped_quantities:
+            if key in datadict:
+                self.raw_mapping[key] = [datadict[key]]
+            else:
+                Exception(f"Mapped property {key} not in datadict.")
+        self.n_sources = np.array(self.raw_mapping[key]).shape[0]
+
 class UnrecognisedProperty(Exception):
     "Error raised when trying to map unrecognised quantities"
     pass
