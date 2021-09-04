3a7ae21131474f30f173f1acc21fb5c8712638a3
James Trayford <jamestrayford@Jamess-MacBook-Pro-2.local>
2021-09-04 21:22:42 +0100

    more utility functions for traversing dictionaries

--- src/strauss/utilities.py
+++ src/strauss/utilities.py
@@ -1,16 +1,37 @@
 
 # a load of utility functions used by STRAUSS
 
-def nested_dict_reassign(original, new):
+def nested_dict_reassign(fromdict, todict):
     """recurse through dictionaries and sub-dictionaries"""
-    for k, v in new.items():
+    for k, v in fromdict.items():
         if isinstance(v, dict):
             # recurse through nested dictionaries
-            nested_dict_reassign(original[k], v)
+            nested_dict_reassign(todict[k], v)
         else:
-            # reassign original value
-            original[k] = v
+            # reassign todict value
+            todict[k] = v
 
+def nested_dict_fill(fromdict, todict):
+    """recurse through dictionaries and sub-dictionaries"""
+    for k, v in fromdict.items():
+        if k not in todict:
+            # assign todict value
+            todict[k] = v
+        elif isinstance(v, dict):
+            # recurse through nested dictionaries
+            nested_dict_fill(todict[k], v)
+            
+def nested_dict_idx_reassign(fromdict, todict, idx):
+    """recurse through dictionaries and sub-dictionaries"""
+    for k, v in fromdict.items():
+        if isinstance(v, dict):
+            # recurse through nested dictionaries
+            nested_dict_idx_reassign(todict[k], v, idx)
+        else:
+            # reassign todict value
+            todict[k] = v[idx]
+
+            
 def const_or_evo_func(x):
     """if x is callable, return x, else provide a function that returns x"""
     if callable(x):
