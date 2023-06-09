9c60e537e3c616ca26bbf8705995606fba993e27
James Trayford <jamestrayford@laptop-8oq7up9j.d.port.ac.uk>
2023-06-09 15:01:04 +0100

    some suggestions for tts implementation

---
+++ src/strauss/tts.py
@@ -0,0 +1,7 @@
+# imports
+
+def render_caption(caption, samplerate):
+    # - render caption as text to speech at correct samplerate
+    # - get the audio as a 1D array of samples (either through saving and reading in a WAV file, or preferably directly somehow)
+    # - return the sample array
+    pass
