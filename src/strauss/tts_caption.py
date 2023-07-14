e79fdf3c66ccefac6128838762d125cd6c64281c
SamYoules <samantha.youles@port.ac.uk>
2023-07-14 13:51:34 +0100

    Moved resample function into utilities

--- src/strauss/tts_caption.py
+++ src/strauss/tts_caption.py
@@ -2,13 +2,14 @@ from TTS.api import TTS
 from scipy.io import wavfile
 from scipy.interpolate import interp1d
 import numpy as np
+import utilities as utils
        
-def render_caption(caption, samplerate, model, caption_path):
+def render_caption(caption, samprate, model, caption_path):
     ''' The render_caption function generates an audio caption from text input
     and writes it as a wav file. If the sample rate of the model is not equal 
     to that passed from sonification.py, it resamples to the correct rate and
     re-writes the file. Text from user input is converted with text-to-speech
-    software from Coqui-AI - https://pypi.org/project/TTS/ . You can view 
+    software from Coqui-AI - https://pypi.org/project/TTS/ . Yohello_u can view 
     publicly available voice models with 'TTS.list_models()'
     '''
     
@@ -17,20 +18,10 @@ def render_caption(caption, samplerate, model, caption_path):
     tts.tts_to_file(text=caption, file_path=caption_path)
     
     # Read the file back in to check the sample rate
-    old_samplerate, old_audio = wavfile.read(caption_path)
+    rate_in, wavobj = wavfile.read(caption_path)
     
     #If it doesn't match the required rate, resample and re-write
-    if old_samplerate != samplerate:
-        duration = old_audio.shape[0] / old_samplerate
-
-        time_old  = np.linspace(0, duration, old_audio.shape[0])
-        time_new  = np.linspace(0, duration, int(old_audio.shape[0] * samplerate / old_samplerate))
-
-        interpolator = interp1d(time_old, old_audio.T)
-        new_audio = interpolator(time_new).T
-
-        wavfile.write(caption_path, samplerate, np.round(new_audio).astype(old_audio.dtype))
-        
-    pass
-    
+    if rate_in != samprate:
+        new_wavobj = utils.resample(rate_in, samprate, wavobj)
+        wavfile.write(caption_path, samprate, new_wavobj)
 
