c133555c73a336b6dacf30b03245de7d0c3eb4e4
Samantha Youles <samantha.youles@port.ac.uk>
2024-07-19 15:19:07 +0100

    Used pathlib for splitting pathname

--- src/strauss/presets/sampler/__init__.py
+++ src/strauss/presets/sampler/__init__.py
@@ -1,10 +1,14 @@
 import glob
 import yaml
+import pathlib
 
-thisdir = '/'.join(__file__.split('/')[:-1])
+##thisdir = '/'.join(__file__.split('/')[:-1])
+path = pathlib.PurePath(__file__)
+thisdir = path.parent
 
 def read_yaml(filename):
-    with open(filename, 'r') as fdata:
+    ##with open(filename, 'r') as fdata:
+    with filename.open(mode='r') as fdata:
         try:
             yamldict = yaml.safe_load(fdata)
         except yaml.YAMLError as err:
@@ -12,22 +16,24 @@ def read_yaml(filename):
     return yamldict
 
 def load_ranges(name="default"):
-    filename = f"{thisdir}/ranges/default.yml"
+    ##filename = f"{thisdir}/ranges/default.yml"
+    filename = pathlib.PurePath(f"{thisdir}","ranges","default.yml")
     return read_yaml(filename)
 
 def load_preset(name="default"):
-    if '/' in name:
+    ##if '/' in name:
+    if pathlib.PurePath(name).name != pathlib.PurePath(name):
         # if open user directly
-        filename = name
+        filename = pathlib.PurePath(name)
     else:
         # else load built-in preset of that name
-        filename = f"{thisdir}/{name}.yml"
+        filename = pathlib.PurePath(f"{thisdir}", f"{name}.yml")
     return read_yaml(filename)
 
 def preset_details(name='*'):
-    pres = glob.glob(f"{thisdir}/*{name.lower()}*.yml")
+    pres = glob.glob(pathlib.PurePath(f"{thisdir}", f"*{name.lower()}*.yml"))
     for p in pres:
-        with open(p, 'r') as fdata:
+        with p.open(mode='r') as fdata:
             try:
                 d = yaml.safe_load(fdata)
                 print(f"\033[1m{d['name']}:\033[0m\n{d['description']}\n")
