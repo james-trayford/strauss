a268e322c31821186b42621e3d64c885063c983d
James Trayford <j.w.trayford@port.ac.uk>
2024-08-21 10:39:32 +0100

    update documentation in src/strauss/stream.py

--- src/strauss/stream.py
+++ src/strauss/stream.py
@@ -1,4 +1,4 @@
-import numpy as np
+>import numpy as np
 import wavio
 import matplotlib.pyplot as plt
 from scipy.signal.windows import hann
@@ -6,9 +6,48 @@ from scipy.signal.windows import hann
 # - implement filter Q-parameter mapping
 
 class Stream:
-    """ Stream object representing audio samples"""
-    def __init__(self, length, samprate=44100, ltype='seconds'):
+    """
+    Stream object representing audio samples.
 
+    Houses audio samples and associates metadata representing the
+    actual audio signal produced by the `Generator` class and output
+    via the `audio_channels` class.
+
+    Attributes:
+      samprate (:obj:`int`): Samples per second of audio stream (Hz)
+      length (:obj:`float`): Duration of the stream in seconds
+      values (:obj:`ndarray`): Values of individual samples
+      samples (:obj:`ndarray`): Indices of each sample
+      samptype (:obj:`ndarray`): Time in seconds each sample occurs
+      buffers (:obj:`Buffers`): Buffered stream if generated
+    
+    Methods:
+      bufferize(bufflength=0.1)
+        wrapper to initialise Buffers subclass
+      consolidate_buffers()
+        wrapper to reassign stream values to consolidated stream
+      filt_sweep(self, ffunc, fmap, qmap=lambda x:x*0 + 0.1,
+                 flo=20, fhi=2.205e4, qlo=0.5, qhi=10)
+        Apply time varying filter to buffered stream
+      get_sampfracs()
+        Get fractional position of the sample in total stream duration
+      save_wav(filename)
+        Save audio stream to wav file, specified by filename
+      reset()
+        Zero audio stream and buffers if present  
+    """
+    def __init__(self, length, samprate=44100, ltype='seconds'):
+        """ 
+        Args:
+          length (numerical): Number representing the length of the
+            stream either as an integer number of samples, or a value
+            of seconds
+          samprate (optional :obj:`int`): Samples per second of audio
+            stream (Hz)
+          ltype (optional :obj:`str`): quantity represented by
+            `length`, either duration in 'seconds' or precise number 
+            of 'samples'
+        """
         # variables we want to keep constant
         self.samprate = samprate
         self._nyqfrq = 0.5*self.samprate
@@ -34,21 +73,42 @@ class Stream:
         self.samptime = self.samples / self.samprate
         
     def bufferize(self, bufflength=0.1):
-        """ wrapper to initialise Buffers subclass """
+        """Wrapper to initialise Buffers subclass
+
+        Args:
+          bufflength (optional, :obj:`float`): duration in seconds of
+            each buffer to be generated
+        """
         self.buffers = Buffers(self, bufflength)
 
     def consolidate_buffers(self):
-        """ wrapper to reassign stream values to consolidated stream """
+        """
+        Wrapper to reassign stream values to consolidated buffers
+
+        See :func:`~stream.Buffers.buffers.to_stream`
+        """
         self.values = self.buffers.to_stream()
         
     def filt_sweep(self, ffunc, fmap, qmap=lambda x:x*0 + 0.1,
                    flo=20, fhi=2.205e4, qlo=0.5, qhi=10):
         """
-        ffunc: function that applies filter 
-        fmap: mapping function representing filter cutoff sweep
-        qmap: mapping function for a filters Q parameter, default: lambda:None
-        flo: lowest frequency of sweep in Hz, default 20
-        fhi: highest frequency of sweep in Hz, default 22.05 kHz
+        Apply time varying filter to buffered stream
+
+        Args:
+          ffunc (function): function that applies filter 
+          fmap (function): mapping function representing filter cutoff
+            sweep
+          qmap (optional, function): mapping function for a filters
+            Q parameter
+          flo (optional, :obj:`float`): lowest frequency of sweep in Hz,
+            default 20
+          fhi (optional, :obj:`float`): lowest frequency of sweep in Hz,
+            default 22.05 kHz
+          qlo (optional, :obj:`float`): lowest 'Q' value of sweep,
+            default 0.5
+          qhi (optional, :obj:`float`): lowest frequency of sweep,
+            default 10
+          
         """
         if not hasattr(self, "buffers"):
             Exception("needs bufferized stream, please run 'bufferize' method first.")
@@ -77,21 +137,57 @@ class Stream:
         self.consolidate_buffers()
 
     def get_sampfracs(self):
+        """ Get fractional position of the sample in total stream duration
+        """
         self.sampfracs = np.linspace(0, 1, self.values.size)
         
     def save_wav(self, filename):
-        """ save audio stream to wav file, specified by filename"""
+        """Save audio stream to wav file, specified by filename
+
+        Args:
+          filename (:obj:`str`): name of output WAV file 
+        """
         wavio.write(filename, self.values, self.samprate, sampwidth=3) 
         
     def reset(self):
-        """ zero audio stream and buffers if present """
+        """Zero audio stream and buffers if present."""
         self.values *= 0.
         if hasattr(self, "buffers"):
             self.buffers.buffs_tile *= 0.
             self.buffers.buffs_olap *= 0.        
         
-class Buffers: 
+class Buffers:
+    """Audio buffers split into uniform discrete chunks or 'buffers'.
+
+    Audio ~:class:`stream.Stream` as a discrete sequence of individual
+    'buffers' of fixed duration (number of samples). This allows time
+    varying operations in frequency space, such as signal filtering.
+    Buffers are tiled in a 'brickwork' fashion so they always overlap
+    with another buffer.
+
+    Attributes:
+      fade (:obj:`ndarray`): Window function for recombining
+        overlapping buffers
+      nsamp_padstream (:obj:`int`): Number of samples needed to split
+        the stream into discrete buffers of chosen length
+      nsamp_pad (:obj:`int`): Number of additional samples needed to
+        add to the original `Stream` size in this case
+      buffs_tile (:obj:`ndarray`): 2d array of buffers completely
+        enclosing the stream (number of buffers x samples per buffer)
+      buffs_olap (:obj:`ndarray`) 2d array of overlap buffers,
+        allowing for cross fading 
+    Methods:
+      to_stream():
+        Reconstruct stream by cross-fading buffers
+    """
     def __init__(self, stream, bufflength=0.1):
+        """
+        Args:
+          stream (~:class:`stream.Stream`): Stream object to be
+            represented using the buffers
+          bufflength (optional, :obj:`float`): duration in seconds of
+            each buffer to be generated
+        """
         nbuff = stream.samprate*bufflength
         if nbuff < 20:
             Exception(f"Error: buffer length {nbuff} samples below "
@@ -115,18 +211,27 @@ class Buffers:
         # pad the stream up to an exact multiple of buffer sample length
         self.nsamp_padstream = self._nbuffs * self._nsamp_buff
         self.nsamp_pad = self.nsamp_padstream-stream._nsamp_stream
-        self.olap_pad = self.nsamp_pad-self._nsamp_halfbuff
-        self.olap_lim = min(stream._nsamp_stream, stream._nsamp_stream+self.olap_pad)
+        self._olap_pad = self.nsamp_pad-self._nsamp_halfbuff
+        self._olap_lim = min(stream._nsamp_stream, stream._nsamp_stream+self.olap_pad)
         
         # construct tile and overlap buffer arrays
         self.buffs_tile = np.pad(stream.values, (0,self.nsamp_pad)
                                  ).reshape((self._nbuffs, self._nsamp_buff))
-        self.buffs_olap = np.pad(stream.values[self._nsamp_halfbuff:self.olap_lim],
-                                 (0,max(0, self.olap_pad))
+        self.buffs_olap = np.pad(stream.values[self._nsamp_halfbuff:self._olap_lim],
+                                 (0,max(0, self._olap_pad))
                                  ).reshape((self._nbuffs-1), self._nsamp_buff)
 
     def to_stream(self):
-        """ reconstruct stream by x-fading buffers """
+        """Reconstruct stream by cross-fading buffers
+
+        Takes the `self.buffs_tile` and `self.buffs_olap` arrays and using
+        the `self.fade` window function, add overlapping sample values
+        together to yield a 1d array of samples.
+
+        Returns:
+          out (:obj:`ndarray`): 1d array of sample values representing the
+            new audio signal for the parent `Stream`. 
+        """
         # apply fades to buffers, first special edge cases...
         self.buffs_tile[0,self._nsamp_halfbuff:] *= self.fade[self._nsamp_halfbuff:]
         self.buffs_tile[-1,:self._nsamp_halfbuff] *= self.fade[:self._nsamp_halfbuff]
@@ -140,6 +245,6 @@ class Buffers:
         padded_stream += self.buffs_tile.flatten()
         flat_olaps    = self.buffs_olap.flatten()
         padded_stream[self._nsamp_halfbuff:-self._nsamp_halfbuff] += flat_olaps
-
+`
         # remove padding on returning reconstructed stream
         return padded_stream[:-self.nsamp_pad]
