8d78b90774d14753075e9dc7d7b4bd5f88ef7a66
James Trayford <j.w.trayford@port.ac.uk>
2024-11-02 22:24:32 -0400

    trying to get pyttsx3 running on my Sonoma 14.2 Mac - works for a first-time run, but leaves the tts engine in an abortive state

--- src/strauss/tts_caption.py
+++ src/strauss/tts_caption.py
@@ -10,19 +10,26 @@ from scipy.interpolate import interp1d
 import numpy as np
 import strauss.utilities as utils
 import re
+import ffmpeg as ff
+import os
 
 try:
     from TTS.api import TTS
     ttsMode = 'coqui-TTS'
 except (OSError, ModuleNotFoundError) as sderr:
-    print('Coqui TTS not found. Trying to import pyttsx3...')
+    # print('Coqui TTS not found. Trying to import pyttsx3...')
     try:
       import pyttsx3
       ttsMode = 'pyttsx3'
-      print('pyttsx3 has been successfully imported.')
+      class TTS:
+          def __init__(*args, **kwargs):
+              pass
+          def list_models(self):
+              getVoices(True)
+      # print('pyttsx3 has been successfully imported.')
     except (OSError, ModuleNotFoundError) as sderr:
       ttsMode = 'None'
-      print('No supported text-to-speech packages have been found.')
+      # print('No supported text-to-speech packages have been found.')
       def TTS(*args, **kwargs):
           raise TTSIsNotSupported("strauss has not been installed with text-to-speech support. \n"
                 "This is not installed by default, due to some specific module requirements of the TTS module.\n"
@@ -102,33 +109,49 @@ def render_caption(caption, samprate, model, caption_path):
           tts.tts_to_file(text=caption, file_path=caption_path)
     
     elif ttsMode == 'pyttsx3':
-      print('Rendering caption (this can take a while if the caption is long)...')
-      
-      # capture stdout from the talkative TTS module
-      with utils.Capturing() as output:
-          # Setup voice model for pyttsx3
-          engine = pyttsx3.init() # initialize object
 
-          # check what model info was set; if none were
-          # specified, use defaults
-          for key in ['rate','volume','voices']:
-              if key in model.keys():
-                engine.setProperty(key, model[key])
+      # Setup voice model for pyttsx3
+      engine = pyttsx3.init() # initialize object
+
+      # check what model info was set; if none were
+      # specified, use defaults
+      for key in ['rate','volume','voices']:
+          if key in model.keys():
+              engine.setProperty(key, model[key])
           else:
-             pass
+              pass
 
-          # render to speech, and write as a wav file (allow )
-          engine.save_to_file(text=caption, filename=caption_path)
+      engine.save_to_file(caption, caption_path)
+
+      try:
+          # TODO: explore why NSS triggers error hear without catching
           engine.runAndWait()
+      except Exception as e:
+          print(e)
+          if engine._inLoop:
+              engine.endLoop()
+          pass
+
+    else:
+       # initialise dummy TTS class to raise error.
+       TTS()
           
     # Read the file back in to check the sample rate
-    rate_in, wavobj = wavfile.read(caption_path)
-    
-    #If it doesn't match the required rate, resample and re-write
+    try:
+        # Try to read in directly...
+        rate_in, wavobj = wavfile.read(caption_path)
+    except:
+        # ...but pttsx3 TTS can produce audio files incompatable
+        # with scipy - convert to standard WAV using ffmpeg
+        cpre = caption_path.split('.')[0] + '_pre.wav'
+        os.rename(caption_path, cpre)
+        ff.input(cpre).output(caption_path).run(quiet=1)
+        rate_in, wavobj = wavfile.read(caption_path)
+
+        
+    # If it doesn't match the required rate, resample and re-write
     if rate_in != samprate:
         new_wavobj = utils.resample(rate_in, samprate, wavobj)
         wavfile.write(caption_path, samprate, new_wavobj)
-    else:
-       TTS()
 
 
